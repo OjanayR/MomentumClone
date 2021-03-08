690c874296e93aab6eabafd4dd7e821bc2febf13
OjanayR <OjanayR@gmail.com>
2021-03-08 18:42:54 -0500

    updated jquery

--- script.js
+++ script.js
@@ -33,29 +33,16 @@ $('.user-name').keypress(function(e) {
 
 });
 
-
-
-
-
-
-    $('.user-name').keypress(function(e) {
-        if(e.which == 13) {
-            var value = e.target.value;
-            if(!value) return;
-            $('.user-name').fadeOut(function(){
-                $('.greeting').html(`Hello ${value}.`);
-                $('.greeting').fadeIn(function(){
-                    setCookie('username', value,365);
-                });
-            });
-        }
-    });
-});
+function setCurrentTime(){
+    var now = new Date();
+    $('.time').html(now.getHours() + ":"+now.getMinutes())
+    $('.date').html(now.toLocaleDateString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric'}));
+}
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
+    var expires = "expires" + toUTCString() ;
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
@@ -65,14 +52,17 @@ function getCookie(cname) {
     var ca = decodedCookie.split(';');
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == '') {
+        while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
-
         if (c.indexOf(name) == 0) {
             return c.substring(name.length, c.length);
         }
     }
-
     return "";
 }
+
+
+
+
+    
